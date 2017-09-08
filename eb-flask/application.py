192ea4c1b73b15b02f5e1cd5feb3b8d215e41f17
noahbragg <noahbragg@cedarville.edu>
2017-09-07 23:43:26 -0400

    made some changes to make the empherial_key route work

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,21 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, session, jsonify
 import stripe
 
 app = Flask(__name__)
 
 stripe.api_key = "sk_test_L9QHJxvNGB737iSYHqkNxR5p"
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
-# token = request.form['stripeToken'] # Using Flask
+token = 123#request.form['stripeToken'] # Using Flask
 
 
-@app.route('/ephemeral_keys', methods = ['GET'])
+@app.route('/ephemeral_keys', methods = ['GET'])    #TODO: should be a post eventually
 def issue_key():
     api_version = request.args['api_version']
-    customerId = 12345 #session['customerId']
+    customerId = retrieveCustomerId()
     key = stripe.EphemeralKey.create(customer = customerId, api_version = "2017-05-25")
     return jsonify(key)
 
-@app.route('/charge', methods = ['POST'])
+@app.route('/charge', methods = ['GET'])            #TODO: should be a post eventually
 def charge():
     amount = request.args['amount']
     source = request.args['source']
@@ -26,10 +27,20 @@ def charge():
         currency = "usd",
         customer = '12345',
         description = "Testing out the charge",
-        source = 123,
+        source = source,
         shipping = shipping)
 
     return "Charge successfully created"
+
+def retrieveCustomerId():
+    if 'customerId' in session:
+        return session['customerId']
+    else:
+        customer = stripe.Customer.create(
+                                            #source=token,
+                                            description="test Customer")
+        session['customerid'] = customer.id
+        return customer.id
 
 # run the app.
 if __name__ == "__main__":
