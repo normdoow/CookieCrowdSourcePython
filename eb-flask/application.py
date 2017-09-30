7f0a5e9783c29dc3037116d0fdb61446832e75be
noahbragg <noahbragg@cedarville.edu>
2017-09-30 18:55:38 -0400

    added emailing through AWS to cook and customer

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, session, jsonify
 import stripe
+import smtplib
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
@@ -21,23 +23,9 @@ def issue_key():
 
 @app.route('/email', methods = ['GET'])
 def email():
-    customer = stripe.Customer.retrieve("cus_BNMtq7s2Ew0lyw")
-    msg = MIMEText("This is a test email from Noah")
-
-    me = "noahbragg@cedarville.edu"
-    you = customer.email
-
-    # me == the sender's email address
-    # you == the recipient's email address
-    msg['Subject'] = 'Test Subject'
-    msg['From'] = me
-    msg['To'] = you
-
-    # Send the message via our own SMTP server.
-    s = smtplib.SMTP('localhost')
-    s.send_message(msg)
-    s.quit()
-    return customer.email
+    sendCustomerEmail("cus_BNMtq7s2Ew0lyw")
+    sendCookEmail("cus_BNMtq7s2Ew0lyw")
+    return "successfully sent email"
 
 @app.route('/charge', methods = ['POST'])
 def charge():
@@ -58,6 +46,8 @@ def charge():
         source = source)
         # shipping = shipping)
 
+    sendCookEmail(customerId)
+    sendCustomerEmail(customerId)
     return "Charge successfully created"
 
 
@@ -71,6 +61,43 @@ def retrieveCustomerId():
         session['customerId'] = customer.id
         return customer.id
 
+def sendCustomerEmail(customerId):
+    customer = stripe.Customer.retrieve(customerId)
+    subject = "Thank you for Purchasing Yummy Hot Cookies!"
+    message = "Thank you for Purchasing Crowd Cookies!\n\nYou will recieve a dozen warm, choclate chip cookies delivered to you in 20 to 30 minutes. If you have any issues, please contact us at noahbragg@cedarville.edu or call (937)-901-6108.\n\n Please share Crowd Cookie with your friends that live in the area! We are just trying out this cool idea to see if people like it. Have them download the app here:\n\n Have a Great Day!\n\nNoah Bragg\nCrowd Cookie Team"
+    sendEmail(customer.email, subject, message)
+
+def sendCookEmail(customerId):
+    customer = stripe.Customer.retrieve(customerId)
+    to = "noahbragg@cedarville.edu,melissajoybragg@gmail.com"
+    subject = "Someone Just Ordered Crowd Cookies!"
+    customerName = str(customer.shipping.name)
+    customerEmail = str(customer.email)
+    customerPhone = str(customer.shipping.phone)
+    customerAddress = "      " + str(customer.shipping.address.line1) + " " + str(customer.shipping.address.line2) + "\n      " + str(customer.shipping.address.city) + ", " + str(customer.shipping.address.state) + "  " + str(customer.shipping.address.postal_code)
+    message = "Customer Name: " + customerName + "\nCustomer Email: " + customerEmail + "\nCustomer Phone: " + customerPhone + "\nDelivery Address: \n" + customerAddress + "\n\nGet those 12 choclate chip cookies made and shipped over to them in 20 to 30 minutes!\n\nSincerely,\nCrowd Cookie Bot"
+    sendEmail(to, subject, message)
+
+def sendEmail(to, subject, message):
+    msg = MIMEText(message)
+
+    sender = "noahbragg@cedarville.edu"
+
+    EMAIL_HOST_USER = "AKIAIME6LLTOUKFDG2IA"
+    EMAIL_HOST_PASSWORD = "AqU75pwvORi2GnG2THYNq5KBbDKMiTCnrI3plq+YRCkw"
+
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = to
+
+    # Send the message via our own SMTP server.
+    s = smtplib.SMTP('email-smtp.us-east-1.amazonaws.com', 587)
+    s.starttls()
+    s.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+    s.sendmail(sender, to, msg.as_string())
+    s.quit()
 
 @app.route('/create_customer', methods = ['GET'])
 def createCustomer():
