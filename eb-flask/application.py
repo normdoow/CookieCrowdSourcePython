3e33d94ce8a29c0c3706e62493df250f09946f7e
noahbragg <noahbragg@cedarville.edu>
2017-12-07 15:50:16 -0500

    made the server work with the database

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,35 +1,39 @@
 from flask import Flask, request, session, jsonify
-# from flask_sqlalchemy import SQLAlchemy
 import stripe
 import smtplib
 from email.mime.text import MIMEText
-from database import database_api
+# from database import database_api
+from flask_sqlalchemy import SQLAlchemy
+from math import radians, cos, sin, asin, sqrt
 # from pusher import Pusher
 # from validate_email import validate_email
 # from email_validator import validate_email, EmailNotValidError
 
 app = Flask(__name__)
-app.register_blueprint(database_api)
-# SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-#     username="noahbragg",
-#     password="CookieDatabase",
-#     hostname="noahbragg.mysql.pythonanywhere-services.com",
-#     databasename="noahbragg$default",
-# )
-# app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-# app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# app.register_blueprint(database_api)
 
-# class Baker(db.Model):
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-#     __tablename__ = "bakers"
+db = SQLAlchemy(app)
 
-#     id = db.Column(db.Integer, primary_key=True)
-#     content = db.Column(db.String(4096))
+class Baker(db.Model):
+
+    __tablename__ = "bakers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    bakerEmail = db.Column(db.String(32))
+    pw = db.Column(db.String(32))
+    lon = db.Column(db.String(32))
+    lat = db.Column(db.String(32))
+    isAvailable = db.Column(db.Boolean(False))
 
 stripe.api_key = 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-
 
 @app.route('/ephemeral_keys', methods = ['POST'])
 def issue_key():
@@ -91,6 +95,31 @@ def charge():
     sendCustomerEmail(customerId, email)
     return "Charge successfully created"
 
+@app.route('/charge_v2', methods = ['POST'])
+def chargev2():
+    amount = request.form['amount']
+    source = request.form['source']
+    customerId = request.form['customer_id']
+    email = request.form['email']
+    bakerEmail = request.form['baker_email']
+
+    if customerId == "":
+        cutomerId = retrieveCustomerId()
+
+    if amount != "0":
+        charge = stripe.Charge.create(
+            amount = amount,
+            currency = "usd",
+            customer = customerId,
+            description = "Testing out the charge",
+            source = source)
+            # shipping = shipping)
+
+    # updateCustomerEmail(customerId, email)        #this didn't seem to do anything
+    sendCookEmailV2(customerId, bakerEmail, email, amount == "600")
+    sendCustomerEmail(customerId, email)
+    return "Charge successfully created"
+
 
 @app.route('/update_customer_address', methods = ['POST'])          #only used on Android right now
 def updateCustomerAddress():
@@ -138,7 +167,7 @@ def retrieveCustomerId():
 def sendCustomerEmail(customerId, email):       #pass in the customer email becasue the shipping one is empty
     customer = stripe.Customer.retrieve(customerId)
     subject = "Thank you for Purchasing Yummy Hot Cookies!"
-    message = "Thank you for Purchasing Crowd Cookies!\n\nYou will receive a dozen warm, chocolate chip cookies delivered to you in around 40 minutes. If you have any issues, please contact us at noahbragg@cedarville.edu or call (937)-901-6108.\n\nPlease share Crowd Cookie with your friends that live in the area! We are just trying out this cool idea to see if people like it. You can find the iOS app or Android app to share with them here: https://itunes.apple.com/us/app/crowd-cookie/id1301846845?mt=8 \n\nhttps://play.google.com/store/apps/details?id=shinzzerz.cookiecrowdsource\n\n Have a Great Day!\n\nNoah Bragg\nCrowd Cookie Team"
+    message = "Thank you for Purchasing Crowd Cookies!\n\nYou will receive a dozen warm, chocolate chip cookies delivered to you in around 40 minutes. If you have any issues, please contact us at noah@crowd-cookie.com or call (937)-901-6108.\n\nPlease share Crowd Cookie with your friends that live in the area! We are just trying out this cool idea to see if people like it. You can find the iOS app or Android app to share with them here: https://itunes.apple.com/us/app/crowd-cookie/id1301846845?mt=8 \n\nhttps://play.google.com/store/apps/details?id=shinzzerz.cookiecrowdsource\n\n Have a Great Day!\n\nNoah Bragg\nCrowd Cookie Team"
     # if validate_email(email, verify=True):      #this is to check to make sure it is a valid email before trying to send to it
     # try:
     #     v = validate_email(email) # validate and get info
@@ -166,10 +195,26 @@ def sendCookEmail(customerId, customerEmail, isFree):
     message = freeText + "Customer Name: " + customerName + "\nCustomer Email: " + customerEmail + "\nCustomer Phone: " + customerPhone + "\nDelivery Address: \n" + customerAddress + "\n\nGet those 12 chocolate chip cookies made and shipped over to them in 40 minutes!\n\nSincerely,\nCrowd Cookie Bot"
     sendEmail(to, subject, message)
 
+def sendCookEmailV2(customerId, bakerEmail, customerEmail, isFree):
+    customer = stripe.Customer.retrieve(customerId)
+
+    to = ["noahbragg@cedarville.edu", "melissajoybragg@gmail.com", "isaiahbragg2020@gmail.com", bakerEmail]
+    subject = "Someone Just Ordered Crowd Cookies!"
+    customerName = str(customer.shipping.name)
+    # customerEmail = str(customer.email)
+    customerPhone = str(customer.shipping.phone)
+    customerAddress = "      " + str(customer.shipping.address.line1) + " " + str(customer.shipping.address.line2) + "\n      " + str(customer.shipping.address.city) + ", " + str(customer.shipping.address.state) + "  " + str(customer.shipping.address.postal_code)
+    freeText = ""
+    if isFree:
+        freeText = "This is a $6 Order!! They got a good deal!\n\n"
+
+    message = freeText + "Baker fulfilling order: " + bakerEmail + "\n\n" + "Customer Name: " + customerName + "\nCustomer Email: " + customerEmail + "\nCustomer Phone: " + customerPhone + "\nDelivery Address: \n" + customerAddress + "\n\nGet those 12 chocolate chip cookies made and shipped over to them in 40 minutes!\n\nSincerely,\nCrowd Cookie Bot"
+    sendEmail(to, subject, message)
+
 def sendEmail(to, subject, message):
     msg = MIMEText(message)
 
-    sender = "noahbragg@cedarville.edu"
+    sender = "noah@crowd-cookie.com"
 
     EMAIL_HOST_USER = ""
     EMAIL_HOST_PASSWORD = ""
@@ -219,10 +264,100 @@ def notifyUsers():
     })
     return "Successfully pushed notification"
 
+
+# database stuff
+@app.route('/change_baker_availability', methods = ['GET'])
+def changeBakerAvailability():
+    bakerEmail = request.args.get('baker_email')
+    isAvailable = request.args.get('is_available') == "Yes"
+
+    baker = Baker.query.filter_by(bakerEmail=bakerEmail).first()
+    baker.isAvailable = isAvailable
+    db.session.commit()
+
+    return "success"
+
+
+@app.route('/login_baker', methods = ['GET'])
+def loginBaker():
+    email = request.args.get('email')
+    pw = request.args.get('pw')
+    bakers = Baker.query.all()
+    isMatch = False
+    for baker in bakers:
+        if email == baker.bakerEmail and pw == baker.pw:
+            isMatch = True
+
+    if isMatch:
+        return "success"
+    return "failure"
+
+@app.route('/cook_available', methods = ['GET'])
+def cookAvailable():
+    userLat = float(str(request.args.get('lat')))
+    userLong = float(str(request.args.get('long')))
+    # latitude = 39.670499      #is inside noah location
+    # longitude = -84.145547
+    # latitude = 39.621895      #is right outside noah location
+    # longitude = -84.128147
+
+    bakers = Baker.query.all()
+    test_points = [{'lat': 39.691483 , 'lng': -84.101717, 'email': "noahbragg@cedarville.edu"}, {'lat': 39.673647, 'lng': -83.977037, 'email': "isaiahbragg@gmail.com"}]    #no and is locations
+
+    radius = 3.5
+    minDist = 50000
+    bakerEmail = ""
+    isCooksAvailable = False
+    isRightLocation = False
+
+    for baker in bakers:
+        dist = haversine(userLong, userLat, float(baker.lon), float(baker.lat))
+        if baker.isAvailable:
+            minDist = min(dist, minDist)
+            if minDist == dist:
+                bakerEmail = baker.bakerEmail
+
+        if dist <= radius:
+            isRightLocation = True
+            if baker.isAvailable:
+                isCooksAvailable = True
+
+
+    if minDist <= radius and bakerEmail != "":
+        return bakerEmail
+
+    # no cooks and wrong location
+    if not isCooksAvailable and not isRightLocation:
+        return "There are no cooks that are making cookies currently. Try again in the evening from 5pm to 9pm. There is more chance that we will be making cookies then! You also must be in a location that is in a 5 mile radius around the Greene to be able to order cookies. Thank you for your patience while we are getting this new business idea up and running!"
+
+    # wrong location
+    if not isRightLocation:
+        return "You must be in a location that is in a 5 mile radius from the Greene for you to be able to order cookies. We will hopefully be coming to a location closer to you soon! Thank you for your patience while we are getting this new business idea up and running!"
+
+    # no cooks
+    return "There are no cooks that are making cookies currently. Try again in the evening from 5pm to 9pm. There is more chance that we will be making cookies then! Thank you for your patience while we are getting this new business idea up and running!"
+
+"""
+Calculate the great circle distance between two points
+on the earth (specified in decimal degrees)
+"""
+def haversine(lon1, lat1, lon2, lat2):
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 3956 # Radius of earth in miles
+    return c * r
+
+
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     app.debug = True
-    # app.run(threaded=True)
-    app.run(host='0.0.0.0')
+    app.run(threaded=True)
+    # app.run(host='0.0.0.0')
--- eb-flask/database.py
+++ eb-flask/database.py
@@ -1,15 +1,16 @@
 from flask import Blueprint, request
 from math import radians, cos, sin, asin, sqrt
+# from flask_sqlalchemy import SQLAlchemy
+# from crowd_cookie_app import db
 
 database_api = Blueprint('database_api', __name__)
-
 
 @database_api.route('/change_baker_availability', methods = ['GET'])
 def changeBakerAvailability():
 	bakerEmail = request.args.get('baker_email')
 	isAvailable = request.args.get('is_available') == "Yes"
 	#TODO: update the database
-	return "successfully updated database"
+	return "success"
 
 
 @database_api.route('/login_baker', methods = ['GET'])
@@ -27,7 +28,8 @@ def cookAvailable():
 	# longitude = -84.145547
 	# latitude = 39.621895		#is right outside noah location
 	# longitude = -84.128147
-	
+
+	bakers = Baker.query.all()
 	#TODO: grab this data from the database
 	test_points = [{'lat': 39.691483 , 'lng': -84.101717, 'email': "noahbragg@cedarville.edu"}, {'lat': 39.673647, 'lng': -83.977037, 'email': "isaiahbragg@gmail.com"}]	#no and is locaitons
 	#TODO: check to see that the baker is available currently as well
@@ -38,30 +40,30 @@ def cookAvailable():
 	#go through the baker locations and get the closest one
 	for point in test_points:
 		dist = haversine(userLong, userLat, point['lng'], point['lat'])
-	
+
 		minDist = min(dist, minDist)
 		if minDist == dist:
 			bakerEmail = point['email']
 
-	if minDist <= radius: 
+	if minDist <= radius:
 		return "inside region, email: " + bakerEmail
 	return "outside region"
 	#either return a baker email because cookies are available or return the string why they are not available
 
 
 """
-Calculate the great circle distance between two points 
+Calculate the great circle distance between two points
 on the earth (specified in decimal degrees)
 """
 def haversine(lon1, lat1, lon2, lat2):
-    # convert decimal degrees to radians 
+    # convert decimal degrees to radians
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
-    # haversine formula 
-    dlon = lon2 - lon1 
-    dlat = lat2 - lat1 
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a)) 
+    c = 2 * asin(sqrt(a))
     r = 3956 # Radius of earth in miles
     return c * r
 
